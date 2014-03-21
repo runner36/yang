e42026632e9fbb09567ce4e44e8ecccf079f50b5
yangjunshuai <runner36@163.com>
2014-03-21 11:47:10 +0800

    first commit

---
+++ www.yangjunshuai.com.yangjunshuai-mvn1-0.0.1-SNAPSHOT/src/main/java/www/yangjunshuai/com/yangjunshuai_mvn1/App.java
@@ -0,0 +1,14 @@
+package www.yangjunshuai.com.yangjunshuai_mvn1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" ); 
+        System.out.println("modified");
+    }
+}
